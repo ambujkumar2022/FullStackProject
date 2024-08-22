c93e3b40d0698ea612d38412958b3c87cd7b2fb9
ambujkumar2022 <108212872+ambujkumar2022@users.noreply.github.com>
2024-08-22 15:18:34 +0200

    Add files via upload

---
+++ fullStack/react-app/src/App.tsx
@@ -0,0 +1,24 @@
+import axios, { AxiosResponse } from 'axios';
+import { useEffect } from 'react';
+import './App.css';
+import {urlWeather} from './endpoints';
+
+function App() {
+
+  useEffect(() => {
+    axios.get(urlWeather)
+      .then((response: AxiosResponse<any>) => {
+        console.log(response.data);
+      })
+  }, [])
+
+  return (
+    <div>
+      <h1>My React app </h1>
+      <p>Communicating with ASP.NET Core</p>
+    </div>
+
+  );
+}
+
+export default App;
